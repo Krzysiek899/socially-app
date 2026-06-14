71986afc1356f85efb75409f8438124532ab5bcb
Pawel <fiz94@op.pl>
2026-06-14 17:33:32 +0200

    Integrate Hotjar in the project to analyze user behavior.

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -8,19 +8,11 @@ import './index.css'
 import App from './App.tsx'
 import { startMockServiceWorker } from './mocks/browser.ts'
 
-// --- Sekcja Inicjalizacji i Diagnostyki GA4 ---
+// --- GA4 Initialization ---
 const gaMeasurementId = import.meta.env.VITE_GA_MEASUREMENT_ID;
-
-console.log("--- DIAGNOSTYKA GA4 ---");
-console.log("1. Wczytany klucz z .env:", gaMeasurementId);
-
 if (gaMeasurementId) {
-  console.log("2. Inicjalizacja ReactGA uruchomiona pomyślnie!");
   ReactGA.initialize(gaMeasurementId);
-} else {
-  console.error("2. BŁĄD: Klucz to 'undefined'. Vite nie widzi pliku .env!");
 }
-// ----------------------------------------------
 
 const bootstrap = async () => {
   if (import.meta.env.DEV) {
